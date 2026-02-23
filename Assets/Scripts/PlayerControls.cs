b72e26d94f43080232f2cd1077b322f19e3a832b
Muhammed Furkan CIRDI <132783338+furkancirdi@users.noreply.github.com>
2026-02-24 02:43:14 +0300

    Touch Controls Update

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
@@ -18,38 +19,51 @@ public class PlayerControls : MonoBehaviour
     {
         mainCam = Camera.main;
 
-        maxLeft = mainCam.ViewportToWorldPoint(new Vector2(0.15f, 0)).x;
-        maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f, 0)).x;
-
-        maxDown = mainCam.ViewportToWorldPoint(new Vector2(0, 0.07f)).y;
-        maxUp = mainCam.ViewportToWorldPoint(new Vector2(0, 0.6f)).y;
+        StartCoroutine(SetBoundaries());
 
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if (Touch.fingers[0].isActive)
+        if (Touch.activeTouches.Count>0)
         {
-            Touch myTouch = Touch.activeTouches[0]; 
-            Vector3 touchPos = myTouch.screenPosition;
-            touchPos = mainCam.ScreenToWorldPoint(touchPos);
 
-            if (Touch.activeTouches[0].phase == TouchPhase.Began) 
+            if (Touch.activeTouches[0].finger.index == 0)
             {
-                offset = touchPos - transform.position; 
+
+                Touch myTouch = Touch.activeTouches[0];
+                Vector3 touchPos = myTouch.screenPosition;
+
+
+#if UNITY_EDITOR
+
+                if (touchPos.x == Mathf.Infinity)
+                    return;
+
+#endif
+
+
+                touchPos = mainCam.ScreenToWorldPoint(touchPos);
+
+                if (Touch.activeTouches[0].phase == TouchPhase.Began)
+                {
+                    offset = touchPos - transform.position;
+                }
+
+                if (Touch.activeTouches[0].phase == TouchPhase.Moved)
+                {
+                    transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+                }
+
+                if (Touch.activeTouches[0].phase == TouchPhase.Stationary)
+                {
+                    transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+                }
+
             }
 
-            if (Touch.activeTouches[0].phase == TouchPhase.Moved) 
-            {
-                transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
-            } 
-
-            if (Touch.activeTouches[0].phase == TouchPhase.Stationary)
-            {
-                transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
-            }
-
+            
             transform.position = new Vector3(
                 Mathf.Clamp(transform.position.x, maxLeft, maxRight), 
                 Mathf.Clamp(transform.position.y, maxDown, maxUp), 
@@ -58,7 +72,7 @@ public class PlayerControls : MonoBehaviour
         }
     }
 
-    private void onEnable()
+    private void OnEnable()
     {
         EnhancedTouchSupport.Enable();
     }
@@ -66,5 +80,16 @@ public class PlayerControls : MonoBehaviour
     private void OnDisable()
     {
         EnhancedTouchSupport.Disable();
+    }
+
+    private IEnumerator SetBoundaries()
+    {
+        yield return new WaitForSeconds(0.4f);
+        
+        maxLeft = mainCam.ViewportToWorldPoint(new Vector2(0.15f, 0)).x;
+        maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f, 0)).x;
+
+        maxDown = mainCam.ViewportToWorldPoint(new Vector2(0, 0.07f)).y;
+        maxUp = mainCam.ViewportToWorldPoint(new Vector2(0, 0.6f)).y;
     }
 }
